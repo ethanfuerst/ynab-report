63c053c698d23f6f014fc401e1593d7631fcc17d
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-01-26 00:21:14 -0500

    fixing mount path

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ modal_image = modal.Image.debian_slim(python_version='3.10').poetry_install_from
     secrets=[modal.Secret.from_name('ynab-report-secrets')],
     mounts=[
         modal.Mount.from_local_dir(
-            'src/sheets/assets', remote_path='/app/src/sheets/assets'
+            'src/sheets/assets/', remote_path='/app/src/sheets/assets'
         )
     ],
     retries=modal.Retries(
